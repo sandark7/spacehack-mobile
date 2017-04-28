d16a483e3d64404bad01b6bc57811c36c57267eb
atsatsin <nuxdie@gmail.com>
2017-04-28 20:47:46 +0300

    add fetch-blob

--- android/app/src/main/java/com/ebay/MainApplication.java
+++ android/app/src/main/java/com/ebay/MainApplication.java
@@ -3,6 +3,7 @@ package com.ebay;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new ReactNativeI18n(),
             new VectorIconsPackage(),
             new RNDeviceInfo(),

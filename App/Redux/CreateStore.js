1c90cbf7394e7ab3c5e6fb76607115205b61fead
atsatsin <nuxdie@gmail.com>
2017-04-28 20:31:01 +0300

    add redux logger & ramda

--- App/Redux/CreateStore.js
+++ App/Redux/CreateStore.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import createLogger from 'redux-logger'
 import { autoRehydrate } from 'redux-persist'
 import Config from '../Config/DebugConfig'
 import createSagaMiddleware from 'redux-saga'
 import RehydrationServices from '../Services/RehydrationServices'
 import ReduxPersist from '../Config/ReduxPersist'
+import R from 'ramda'
 
 // creates the store
 export default (rootReducer, rootSaga) => {
@@ -17,6 +19,21 @@ export default (rootReducer, rootSaga) => {
   const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null
   const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
   middleware.push(sagaMiddleware)
+
+  /* ------------- Logger Middleware ------------- */
+
+  // remove common noise
+  const loggingBlacklist = ['EFFECT_TRIGGERED', 'EFFECT_RESOLVED', 'EFFECT_REJECTED', 'persist/REHYDRATE']
+  if (__DEV__) {
+    // the logger master switch
+    const USE_LOGGING = Config.reduxLogging
+    // silence these saga-based messages
+    // create the logger
+    const logger = createLogger({
+      predicate: (getState, { type }) => USE_LOGGING && R.not(R.contains(type, loggingBlacklist))
+    })
+    middleware.push(logger)
+  }
 
   /* ------------- Assemble Middleware ------------- */
 
